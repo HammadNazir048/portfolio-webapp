423abea00c47989cbcd8a9f72d6296c2535ebc38
Muhammad Hammad Nazir <116563563+HammadNazir048@users.noreply.github.com>
2024-12-27 09:04:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Muhammmad Hammad Nazir",
+  name: "Muhammmad Hammad Nazir + test",
   title: "Mern Stack Developer",
   email: "mhn048c@gmail.com",
   gitHub: "HammadNazir048",
